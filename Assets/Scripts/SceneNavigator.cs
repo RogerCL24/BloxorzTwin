c5191be471d1e1c696ab244a15f52e47c40b8610
RogerCL <roger.cot.lon@gmail.com>
2025-12-08 17:54:23 +0100

    Boton de salida del menu implementado

--- Assets/Scripts/SceneNavigator.cs
+++ Assets/Scripts/SceneNavigator.cs
@@ -27,5 +27,8 @@ public class SceneNavigator : MonoBehaviour
     {
         Debug.Log("Saliendo del juego...");
         Application.Quit();
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
